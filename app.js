cd4314291a1bdcb7aa3d75d64c4d43c7637b7606
MaarcoBruno <marco.casla@hotmail.com>
2022-12-21 01:01:26 -0300

    fix

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.listen(process.env.PORT || 3000, function() {
   });
 
 app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'views\home.html'));
+    res.sendFile(path.join(__dirname+ 'views\home.html'));
 });
 
 
